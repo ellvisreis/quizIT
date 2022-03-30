f9fe4d77b3fe7e4e2dc4efedff6c4e0c9b8b5085
Ellvis <tecnologia@geodigital.tech>
2022-03-30 09:16:13 -0300

    primeiro commit Ellvis

---
+++ app/src/main/java/com/augusto/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.augusto.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+        private Button buttonTestar1;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        buttonTestar1 = findViewById(R.id.buttonTestar1);
+
+        buttonTestar1.setOnClickListener(new.ViewOnClickListener(){
+            @Override
+                    public void onClick(View view){
+
+            }
+        });
+
+
+
+                
+    }
+}
