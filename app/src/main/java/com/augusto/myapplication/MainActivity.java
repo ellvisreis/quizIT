b1313bbeba70fbad6a9f9a79c68db23a3549e3f1
Ellvis <tecnologia@geodigital.tech>
2022-06-27 00:26:18 -0300

    Commit para teste versao

--- app/src/main/java/com/augusto/myapplication/MainActivity.java
+++ app/src/main/java/com/augusto/myapplication/MainActivity.java
@@ -9,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
 
         private Button buttonTestar1;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
